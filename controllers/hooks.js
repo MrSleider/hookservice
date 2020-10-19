4a7a80958685848bf90802ac509707ecac04bbca
MrSleider <thegamingesp56@gmail.com>
2020-10-19 22:18:07 +0200

    Added basic saving functionallitie

--- controllers/hooks.js
+++ controllers/hooks.js
@@ -3,19 +3,37 @@ var Webhook = require('../models/webhook.model.js');
 
 trigger_webhook = async (req, res) => {
 
-  if (!req.hook) {
-    return res.status(400).send({ message:'Something is wrong with the hook' });
+  if ( !req.hook || req.hook_error ) {
+    return res.status(400).send({ message: req.hook_error || 'Webhook not found!', hook: req.params.id });
   }
 
   const hook  = req.hook;
-  var   data  = [];
 
-  for ( let query of hook.queries) {
-    var model = createSchema(query.collection);
-    data.push(await model.find(getFilterParams(query.filters) || {}));
+  if ( hook.queries ) {
+    switch ( hook.action ) {
+      case 'GET':
+        var data = await GET_DATA( hook.queries );
+        return res.status(200).send({ message: 'The data has been found!', data: data });
+        break;
+      case 'SAVE':
+        var input_data    = req.body.data; // We must verify this is a well crafted json object
+        const collections  = hook.queries.map( q => { return q.collection }).join(', ');
+
+        if ( await SAVE_DATA(hook.queries, input_data)) {
+          return res.status(200).send({ message: 'The data has been saved!', saved_data: input_data, collections: collections });
+        } else {
+          return res.status(500).send({ message: 'Could not save the data!'});
+        }
+        break;
+      case 'UPDATE':
+        break;
+      case 'REMOVE':
+        break;
+      default:
+        console.error(`[!] ERROR: Webhook with ID: ${hook._id} does not have an action!`);
+        return res.status(500).send({ message: 'Bad webhook configuration! No action found.' });
+    }
   }
-
-  return res.status(200).send({ message: 'The data has been found!', data: data });
 }
 
 
@@ -50,6 +68,40 @@ configure_connector = (req, res, next) => {
 
 }
 
+
+// Hooks CRUD
+
+async function GET_DATA    ( queries ) {
+  var   data  = [];
+
+  for ( let query of queries) {
+    var model = createSchema(query.collection);
+    data.push(await model.find(getFilterParams(query.filters) || {}));
+  }
+
+  return data;
+}
+
+async function SAVE_DATA    ( queries, data ) {
+  // Returns a boolean
+  var flag = false;
+
+  for ( let query of queries ) {
+    var model = createSchema(query.collection);
+    var collection = new model();
+    console.log( await collection.save(data));
+  }
+
+  return flag;
+}
+
+function UPDATE_DATA () {
+
+}
+
+function REMOVE_DATA () {
+
+}
 
 function createSchema(collection) {
   // This method returns an empty schema in order to use it for searching collections
